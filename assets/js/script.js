cb35ebf768c2c42424ca72e1d957e128b8e4a742
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-04 22:47:48 -0300

    js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+console.log("Calculando orçamento");
+
+
+//Manipulção do DOM
+//console.log (document.querySelector("#qtde").value)
+
+document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
+document.querySelector("#js").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_sim").addEventListener("change", calcularOrcamento)
+document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
+
+
+function calcularOrcamento(){
+   let preco = qtde.value *100
+   
+   if (js.checked) preco = preco * 1.1
+
+   if(layout_sim.checked) preco += 500
+
+   
+   let taxaDeUrgencia = preco *(1.1 - prazo.value *0.1)
+   preco += taxaDeUrgencia
+   
+   label_prazo.innerHTML = `Prazo (${prazo.value}  semanas)`
+
+   output.innerHTML = "R$" + preco.toFixed(2)
+
+   
+}
