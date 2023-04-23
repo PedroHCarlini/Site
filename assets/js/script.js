7694e2bce7e08beac1b1a69567b5d1c28c85be81
PedroHCarlini <phcarlini@gmail.com>
2023-04-23 19:43:21 -0300

    Respondividade 3.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,17 @@ document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
 
 
 function calcularOrcamento(){
-   let preco = qtde.value *100
+   let preco = qtde.value *2200
    
-   if (js.checked) preco = preco * 1.1
+   if (js.checked) preco = preco * 2
 
-   if(layout_sim.checked) preco += 500
+   if(layout_sim.checked) preco += 5000
 
    
    let taxaDeUrgencia = preco *(1.1 - prazo.value *0.1)
    preco += taxaDeUrgencia
    
-   label_prazo.innerHTML = `Prazo (${prazo.value}  semanas)`
+   label_prazo.innerHTML = `Prazo (${prazo.value}  anos)`
 
    output.innerHTML = "R$" + preco.toFixed(2)
 
